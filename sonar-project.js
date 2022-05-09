9a46340d664b2c8ea04d5b913104124a3c9c956c
KOLLI PAVAN <102366432+kollipavan@users.noreply.github.com>
2022-05-09 19:46:45 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -8,7 +8,7 @@ sonarqubeScanner({
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    'sonar.login': 'ad6638b23dd6e7923f2e4139e607a671d442a56a',
 	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
+	    //'sonar.password': 'admin123',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
